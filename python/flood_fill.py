1bd1dd9ba80711bdf6f299521f9d3eaf26818f4e
Rodrigo Moran <rigo.moran0@gmail.com>
2024-10-16 20:32:53 -0400

    add: flood fill problem

--- python/flood_fill.py
+++ python/flood_fill.py
@@ -2,6 +2,32 @@ from typing import List
 
 
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        return [[0]]
-        
+    def floodFill(
+        self, image: List[List[int]], sr: int, sc: int, color: int
+    ) -> List[List[int]]:
+        if image[sr][sc] == color:
+            return image
+
+        start_color = image[sr][sc]
+        queue = [(sr, sc)]
+        while queue:
+            x, y = queue.pop(0)
+
+            # print(f"Step: ({x},{y})\n")
+            if x >= 0 and y >= 0:
+                image[x][y] = color
+            if x + 1 < len(image):
+                if image[x + 1][y] == start_color:
+                    queue.append((x + 1, y))
+            if x - 1 >= 0:
+                if image[x - 1][y] == start_color:
+                    queue.append((x - 1, y))
+            if y + 1 < len(image[0]):
+                if image[x][y + 1] == start_color:
+                    queue.append((x, y + 1))
+            if y - 1 < len(image):
+                if image[x][y - 1] == start_color:
+                    queue.append((x, y - 1))
+            # print(f"Step: ({x},{y})\n", image[0], "\n", image[1], "\n", image[2])
+
+        return image
--- python/flood_fill_test.py
+++ python/flood_fill_test.py
@@ -23,6 +23,34 @@ class TestSolution(unittest.TestCase):
                 color=2,
                 expected=[[2, 2, 2], [2, 2, 0], [2, 0, 1]],
             ),
+            Case(
+                image=[[1, 1, 1, 1], [1, 1, 1, 0], [1, 0, 1, 1]],
+                sr=1,
+                sc=1,
+                color=2,
+                expected=[[2, 2, 2, 2], [2, 2, 2, 0], [2, 0, 2, 2]],
+            ),
+            Case(
+                image=[[0, 0, 0], [0, 0, 0]],
+                sr=0,
+                sc=0,
+                color=0,
+                expected=[[0, 0, 0], [0, 0, 0]],
+            ),
+            Case(
+                image=[[0,0,0],[0,1,0]],
+                sr=0,
+                sc=0,
+                color=2,
+                expected=[[2,2,2],[2,1,2]],
+            ),
+            Case(
+                image=[[0,0,1],[1,0,1]],
+                sr=0,
+                sc=0,
+                color=2,
+                expected=[[2,2,1],[1,2,1]],
+            ),
         ]
 
     def test_flood_fill(self):
